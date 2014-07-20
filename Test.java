2d573589c44b01f11807430d24d63b77b3b9274c
Amarto <amarto.rajaram@gmail.com>
2014-07-20 09:32:59 -0400

    removed unnecessary scanner from test class

--- Test.java
+++ Test.java
@@ -10,11 +10,7 @@ public class Test {
 		
 		MatrixBuilder builder = new MatrixBuilder();
 		
-//		Scanner in = new Scanner(System.in);
-//		System.out.println("Please enter the number of rows: ");
-//		int rows = in.nextInt();
-//		System.out.println("Please enter the number of columns: ");
-//		int columns = in.nextInt();
+
 		
 		Matrix thisMatrix = builder.buildMatrix();
 		RrefCalculator rrefcalc = new RrefCalculator();
